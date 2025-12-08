2159922fc79cd88ea94b0bd780e4247c8c664501
slam88d-beep <slam88d@gmail.com>
2025-12-08 17:33:20 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -685,8 +685,10 @@ with tab_dealer:
     # ② 렌탈 모델
     st.subheader(t_sec_rental)
 
-    initial_invest_rental = initial_unit_cost
-    annual_profit_rental = dealer_rental_fee * 12.0  # 서비스비 포함으로 가정
+initial_invest_rental = initial_unit_cost
+# 렌탈 연간 순이익 = 연간 렌탈료 - 연간 서비스 제공 원가
+annual_profit_rental = dealer_rental_fee * 12.0 - service_cost_year
+
 
     payback_years_dealer = None
     if annual_profit_rental > 0:
